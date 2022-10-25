2ef01b01aef55a4541c677ac383ae99ea5fd5090
craftablescience <me@craftablescience.info>
2022-10-25 03:09:50 -0400

    feat: vs2022 support

--- src/public/tier0/memoverride.cpp
+++ src/public/tier0/memoverride.cpp
@@ -278,7 +278,7 @@ extern "C"
         return pMemOut;
     }
 
-    size_t _msize_base(void* pMem)
+    size_t _msize_base(void* pMem) _CRT_NOEXCEPT
     {
         return g_pMemAlloc->GetSize(pMem);
     }
